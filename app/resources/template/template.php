7469eef536e141df05eeb23e07de4e7991ae5cd9
mhuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-05-06 13:58:44 -0300

    Archivo template.php

---
+++ app/resources/template/template.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <?php
+    require_once "includes/head.php";
+    ?>
+</head>
+<body>
+    <header>
+    <?php
+
+    ?>
+    </header>
+    <main>
+    <?php
+
+    ?>
+    </main>
+</body>
+</html>
