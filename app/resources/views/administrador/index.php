12ebf4739a9964d73e1f7ab80dd49ff2aebb5b10
mhuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-05-06 14:00:20 -0300

    Los index para cada usuario y autenticación

---
+++ app/resources/views/administrador/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+    require_once "app/resources/template/includes/head.php";
+    ?>
+</head>
+<body>
+    <p>inicio admin</p>
+</body>
+</html>
