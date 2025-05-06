63a0c744cad8516a8939280e779b26b9b863ea57
mhuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-05-06 13:59:07 -0300

    Archivos footer y head

---
+++ app/resources/template/includes/head.php
@@ -0,0 +1,7 @@
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <base href="http://localhost/ldp_proyecto_cine_huenchur/">
+    <link href="public/assets/libs/bootstrap-5.3.5-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="public/assets/css/custom.css">
+    <script defer src="public/assets/libs/bootstrap-5.3.5-dist/js/bootstrap.bundle.min.js"></script>
+    <title>LDP CINE</title>
