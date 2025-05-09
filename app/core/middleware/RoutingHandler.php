5dbc1e528192707043262ce4b6b2f7a5e0e4b973
mhuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-05-09 16:27:38 -0300

    Modificación del RoutingHandler

--- app/core/middleware/RoutingHandler.php
+++ app/core/middleware/RoutingHandler.php
@@ -11,9 +11,12 @@ class RoutingHandler extends AbstractHandler{
     {
         $controllerName = 'app\\core\\controller\\'. ucfirst($request->getController()) .'Controller';
 
+        if (!class_exists($controllerName) || !method_exists($controllerName, $request->getAction())) {
+            //throw new \Exception("controlador y/o accion incorrectos");
+        }
         call_user_func_array(array(new $controllerName, $request->getAction()), array($request, $response));
 
-        echo "Hola soy el Routing :D <br>\n";
+        //echo "Hola soy el Routing :D <br>\n";
         return parent::handle($request, $response);
     }
 }
