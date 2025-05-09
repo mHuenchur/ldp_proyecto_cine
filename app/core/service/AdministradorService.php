01e341ae27ef411b6c11e54e5a8cb0a00b59ee39
mhuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-05-09 16:31:20 -0300

    Creación clases de servicio autenticación, administrador, operador y externo

---
+++ app/core/service/AdministradorService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace app\core\service;
+
+use app\core\service\base\Service;
+use app\core\service\base\InterfaceService;
+
+
+final class AdministradorService extends Service implements InterfaceService{
+    
+    public function save(array $object): void{
+
+    }
+
+    public function load($id): InterfaceDTO{
+
+    }
+
+    public function update(array $object): void{
+
+    }
+
+    public function delete($id): void{
+
+    }
+
+    public function list(): array{
+        
+    }
+}
