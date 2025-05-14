37fb290e50544db8433827fe6f1d9737a8761a81
mhuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-05-14 11:02:12 -0300

    Archivo vista para admininstrador, sala y pelicula

--- app/resources/views/administrador/index.php
+++ app/resources/views/administrador/index.php
@@ -1,11 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <?php
-    require_once "app/resources/template/includes/head.php";
-    ?>
-</head>
-<body>
-    <p>inicio admin</p>
-</body>
-</html>
+<p>administrador -> inicio</p>
---
+++ app/resources/views/sala/index.php
@@ -0,0 +1,1 @@
+<p>sala -> index</p>
