b6aa45240bc51228b52cc457283397a2803883ce
mhuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-05-14 10:31:23 -0300

    Modificación template

--- app/resources/template/template.php
+++ app/resources/template/template.php
@@ -2,19 +2,21 @@
 <html lang="es">
 <head>
     <?php
-    require_once "includes/head.php";
+    require_once APP_TEMPLATE . "includes/head.php";
     ?>
 </head>
 <body>
     <header>
     <?php
-
+    require_once APP_TEMPLATE . "includes/administradorHeader.php";
     ?>
     </header>
     <main>
     <?php
-
+    require_once APP_VIEWS . $this->view;
     ?>
     </main>
+    <footer>
+    </footer>
 </body>
 </html>
