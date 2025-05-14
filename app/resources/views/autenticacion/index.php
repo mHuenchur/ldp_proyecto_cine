f6020b3a8ec55a6d41e3c9399b632537597ef5e2
mhuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-05-14 10:30:34 -0300

    Cambio en el index de auenticacion

--- app/resources/views/autenticacion/index.php
+++ app/resources/views/autenticacion/index.php
@@ -1,11 +1,22 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <?php
-    require_once "app/resources/template/includes/head.php";
-    ?>
-</head>
-<body>
-    <p>inicio autenticacion</p>
-</body>
-</html>
+<?php 
+
+use app\core\model\dao\UsuarioDAO;
+use app\core\model\dto\UsuarioDTO;
+use app\libs\connection\Connection;
+
+/* $data = ["id"=>"67", "nombre" => "Manolo","apellido" => "Pierre","nombre_usuario" => "mPierre",
+"contrasena"=>"qwer123","correo"=>"mPierre@gmail.com","id_perfil"=>"2"];
+
+
+try {
+    $conn = new Connection();
+    $pdo = $conn->get();
+    $dto = new UsuarioDTO($data);
+    $dao = new UsuarioDAO($pdo);
+    $dao->save($dto);
+    echo "Se agregó exitosamente";
+} catch (\Exception $ex) {
+    echo $ex->getMessage();
+} */
+?>
+<p>autenticacion -> index</p>
