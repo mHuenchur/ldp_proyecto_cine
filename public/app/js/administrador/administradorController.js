279e491ed4effd3b9c4a31f184e673eb6a99e119
mhuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-05-15 16:17:11 -0300

    Controlador y servicio js de la sala para el administrador

---
+++ public/app/js/administrador/administradorController.js
@@ -0,0 +1,52 @@
+let administradorController = {
+    data: {
+        id: "0",
+        capacidad: "0",
+        numeroSala: "0",
+        estado: "0"
+    },
+    save: () => {
+        let form = document.forms["formularioAltaSala"];
+
+        if (form.checkValidity()){
+            if (form.salaCapacidad.value.trim() === "" || form.salaNumero.value.trim() === "") {
+                administradorController.showMessage("Existen campos vacios!");
+            }else{
+                administradorController.data.capacidad = form.salaCapacidad.value;
+                administradorController.data.numeroSala = form.salaNumero.value;
+                if(document.getElementById("habilitada").checked){
+                    administradorController.data.estado = form.habilitada.value;
+                }else{
+                    administradorController.data.estado = form.inhabilitada.value;
+                }
+                administradorService.save(administradorController.data)
+                .then(response => {
+                    console.log("Respuesta del servidor", response)
+                    if(response.error == ""){
+                        administradorController.showMessage(response.mensaje);
+                        form.reset();
+                    }
+                    else{
+                        administradorController.showMessage(response.error);
+                    }
+                })
+            }
+        }
+    },
+    showMessage: (respuesta) => {
+        const toastLiveExample = document.getElementById('liveToast')
+        const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+        const message = document.getElementById("messageContainer");
+        message.innerHTML = respuesta;
+        toastBootstrap.show()
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    let btnNewSala = document.getElementById("btnGuardarSala");
+    if(btnNewSala != null){
+        btnNewSala.onclick = () => {
+            administradorController.save();
+        }
+    }
+})
---
+++ public/app/js/administrador/administradorService.js
@@ -0,0 +1,21 @@
+let administradorService = {
+    save: (data) => {
+        return fetch("sala/save", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => {
+            if(!response.ok){
+                throw new Error(response.status);
+            }
+            return response.json()
+        })
+        .catch(error => {
+            console.error("ERROR EN LA PETICION ", error)
+        });
+    }
+}
