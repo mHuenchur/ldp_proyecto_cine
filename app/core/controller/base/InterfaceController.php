36f692193b191f8e73099fc083b1010c072359ea
mhuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-05-15 16:26:16 -0300

    Modificaciones pequeñas

--- app/core/controller/base/InterfaceController.php
+++ app/core/controller/base/InterfaceController.php
@@ -2,19 +2,22 @@
 
 namespace app\core\controller\base;
 
+use app\libs\request\Request;
+use app\libs\response\Response;
+
 interface InterfaceController{
 
-    public function index(): void;
+    public function index(Request $request, Response $response): void;
 
-    public function load(): void;
+    public function load(Request $request, Response $response): void;
 
-    public function create(): void;
+    public function create(Request $request, Response $response): void;
 
-    public function save(): void;
+    public function save(Request $request, Response $response): void;
 
-    public function edit(): void;
+    public function edit(Request $request, Response $response): void;
 
-    public function update(): void;
+    public function update(Request $request, Response $response): void;
 
-    public function delete(): void;
+    public function delete(Request $request, Response $response): void;
 }
