cbcc260a2c7257b7085f53137d2c4f32a90fd70a
mhuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-05-15 16:23:45 -0300

    Archivo SalaDAO

---
+++ app/core/model/dao/SalaDAO.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace app\core\model\dao;
+
+use app\core\model\base\DAO;
+use app\core\model\base\InterfaceDAO;
+use app\core\model\base\InterfaceDTO;
+use app\core\model\dto\SalaDTO;
+
+final class SalaDAO extends DAO implements InterfaceDAO
+{
+    public function __construct($conn)
+    {
+        parent::__construct($conn, "sala");
+    }
+
+    public function save(InterfaceDTO $object): void
+    {
+        $this->validarCampos($object);
+        $sql = "INSERT INTO {$this->table} VALUES (DEFAULT, :capacidad, :numeroSala, :estado)";
+        $stmt = $this->conn->prepare($sql);
+        $data = $object->toArray();
+        unset($data["id"]);
+
+
+        $stmt->execute($data);
+    }
+
+    public function load($id): InterfaceDTO
+    {
+        return  new InterfaceDTO();
+    }
+
+    public function update(InterfaceDTO $object): void
+    {
+        
+    }
+
+    public function delete($id): void
+    {
+        
+    }
+
+    public function list(): array
+    {
+        return array();
+    }
+    //VALIDACIONES
+    public function validarCampos(SalaDTO $object){
+        if($object->getNumeroSala() == "" || $object->getCapacidad() == "" || $object->getEstado() == ""){
+            throw new \Exception("Campos con valores incorrectos!");
+        }
+    }
+}
+
+
+?>
