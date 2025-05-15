39f7923e2fbc88a13648b3544faec09678e495c2
mhuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-05-15 16:16:25 -0300

    Archivo test de php

---
+++ test/test.php
@@ -0,0 +1,6 @@
+<?php
+//PARA TENER A LA FECHA Y TIEMPO COMO VALORES SEPARADOS DEL DATETIME
+//SELECT DATE(fecha_inicio) as mydate, TIME(fecha_inicio) as mytime FROM `horario`; 
+
+//LO MISMO SIN EL NOMBRE DE VARIABLE
+//SELECT DATE(column), TIME(column) FROM table
