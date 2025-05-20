1aa2d443a2bb0511b005044cd867c65fa4eb881d
mhuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-05-20 20:07:39 -0300

    Modificación vista index autenticacion

--- app/resources/views/autenticacion/index.php
+++ app/resources/views/autenticacion/index.php
@@ -1,22 +1,7 @@
-<?php 
-
-use app\core\model\dao\UsuarioDAO;
-use app\core\model\dto\UsuarioDTO;
-use app\libs\connection\Connection;
-
-/* $data = ["id"=>"67", "nombre" => "Manolo","apellido" => "Pierre","nombre_usuario" => "mPierre",
-"contrasena"=>"qwer123","correo"=>"mPierre@gmail.com","id_perfil"=>"2"];
-
-
-try {
-    $conn = new Connection();
-    $pdo = $conn->get();
-    $dto = new UsuarioDTO($data);
-    $dao = new UsuarioDAO($pdo);
-    $dao->save($dto);
-    echo "Se agregó exitosamente";
-} catch (\Exception $ex) {
-    echo $ex->getMessage();
-} */
-?>
-<p>autenticacion -> index</p>
+<div class="container-fluid text-center">
+    <p>AUTENTICACION -> INDEX</p>
+    <h1>ADMINISTRADOR DE CINE</h1>
+</div>
+<div class="container-fluid text-center">
+  <p>AQUI CARTELERA VIGENTE</p>
+</div>
