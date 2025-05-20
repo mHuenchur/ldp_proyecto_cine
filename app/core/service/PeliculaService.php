096cf2f14c43a69df571a614ff0da0ddd0129a11
mhuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-05-20 20:13:09 -0300

    Archivo PeliculaService

---
+++ app/core/service/PeliculaService.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace app\core\service;
+
+use app\core\service\base\Service;
+use app\core\service\base\InterfaceService;
+use app\libs\connection\Connection;
+use app\core\model\dao\PeliculaDAO;
+use app\core\model\dto\PeliculaDTO;
+use app\core\model\base\InterfaceDTO;
+
+
+final class SalaService extends Service implements InterfaceService{
+    
+    public function save(array $object): void{
+        $conn = Connection::get();
+        $dao = new PeliculaDAO($conn);
+        $dao->save(new PeliculaDTO($object));
+    }
+
+    public function load($id): InterfaceDTO{ 
+        return new InterfaceDTO;
+    }
+
+    public function update(array $object): void{
+
+    }
+
+    public function delete($id): void{
+
+    }
+
+    public function list(): array{
+        return array();
+    }
+}
