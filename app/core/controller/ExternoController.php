f2ddc0667a9a9448df6ee3f9177b0de18f91b43b
mhuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-05-20 20:18:52 -0300

    Codigo para ExternoController

---
+++ app/core/controller/ExternoController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace app\core\controller;
+
+use app\core\controller\base\InterfaceController;
+use app\core\controller\base\Controller;
+use app\libs\response\Response;
+use app\libs\request\Request;
+
+final class ExternoController extends Controller implements InterfaceController{
+
+    public function index(Request $request, Response $response): void{
+        $this->view = "externo/index.php";
+        require_once APP_TEMPLATE . "template.php";
+    }
+    public function load(Request $request, Response $response): void{
+
+    }
+
+    public function create(Request $request, Response $response): void{
+
+    }
+
+    public function save(Request $request, Response $response): void{
+
+    }
+
+    public function edit(Request $request, Response $response): void{
+
+    }
+
+    public function update(Request $request, Response $response): void{
+
+    }
+
+    public function delete(Request $request, Response $response): void{
+        
+    }
+}
