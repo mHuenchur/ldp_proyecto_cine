514138927b966b551d6c3e658e28ab56b2e83d67
mhuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-05-24 20:20:04 -0300

    Funcionalidad para el authentication handler

--- app/core/handler/AuthenticationHandler.php
+++ app/core/handler/AuthenticationHandler.php
@@ -9,8 +9,18 @@ class AuthenticationHandler extends AbstractHandler{
 
     public function handle(Request $request, Response $response)
     {
+        
+        /* session_start(); */
 
-        //echo "Hola soy el Authentication :D <br>\n";
+        //si no esta el token o  el token no es app_token
+        if(!isset($_SESSION["token"]) || $_SESSION["token"] !== APP_TOKEN){
+            //si el controlador no es autenticaion o  la accion no es login
+            if($request->getController() !== "autenticacion" || $request->getAction() !== "login"){
+                $request->setController("autenticacion");
+                $request->setAction("index");
+            }
+        }
+
         return parent::handle($request, $response);
     }
 }
