f4b30586e8fa4ee9669a1bed605c226a9b49c190
mhuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-06-06 17:00:21 -0300

    Controlador para las funciones

---
+++ app/core/controller/FuncionController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace app\core\controller;
+
+use app\core\controller\base\InterfaceController;
+use app\core\controller\base\Controller;
+use app\core\service\FuncionService;
+use app\libs\response\Response;
+use app\libs\request\Request;
+
+final class FuncionController extends Controller implements InterfaceController{
+    
+    public function __construct()
+    {
+        parent::__construct([
+            "public/app/js/administrador/operadorController.js",
+            "public/app/js/administrador/operadorService.js"
+        ]);
+    }
+
+    public function index(Request $request, Response $response): void{
+        $this->view = "funcion/index.php";
+        require_once APP_TEMPLATE . "template.php";
+    }
+    public function load(Request $request, Response $response): void{
+
+    }
+
+    public function create(Request $request, Response $response): void{
+
+    }
+
+    public function save(Request $request, Response $response): void{
+        
+    }
+
+    public function edit(Request $request, Response $response): void{
+
+    }
+
+    public function update(Request $request, Response $response): void{
+
+    }
+
+    public function delete(Request $request, Response $response): void{
+        
+    }
+}
