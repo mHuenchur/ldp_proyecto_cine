6fe93f0ae8ca055ccbc76206e3c4f93ee63913ac
mhuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-06-06 17:02:53 -0300

    Se implementó el AuthorizationHandler

--- app/core/handler/AuthorizationHandler.php
+++ app/core/handler/AuthorizationHandler.php
@@ -9,8 +9,27 @@ class AuthorizationHandler extends AbstractHandler{
 
     public function handle(Request $request, Response $response)
     {
+        
+        if(isset($_SESSION["token"]) && $_SESSION["token"] === APP_TOKEN){
+            //print_r($request->getController());
+            $contAdmitidosOperador = ["operador", "cartelera", "funcion", "autenticacion"];
+            $contAdmitidosAdministrador = ["administrador", "sala", "pelicula", "usuario", "autenticacion"];
+            $contAdmitidosExterno = ["externo", "autenticacion"];
+            if ($_SESSION["perfil"] == "administrador" && !in_array($request->getController(), $contAdmitidosAdministrador)) {
+                throw new \Exception("ACCESO DENEGADO PARA ADMINISTRADOR");
+            } else {
+                if ($_SESSION["perfil"] == "operador" && !in_array($request->getController(), $contAdmitidosOperador)) {
+                    throw new \Exception("ACCESO DENEGADO PARA OPERADOR");
+                } else {
+                    if ($_SESSION["perfil"] == "externo" && !in_array($request->getController(), $contAdmitidosExterno)) {
+                        throw new \Exception("ACCESO DENEGADO PARA EXTERNO");
+                    }
+                }
+                
+            }
+        }
 
-        //echo "Hola soy el Authorization :D <br>\n";
+
         return parent::handle($request, $response);
     }
 }
