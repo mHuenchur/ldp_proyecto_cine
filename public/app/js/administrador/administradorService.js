26b6cef4b570185a3dd250dac05c5d5412fa5ab8
mhuenchur <102182282+mHuenchur@users.noreply.github.com>
2025-06-06 18:03:01 -0300

    Implementación JS service para enviar los datos de una pelicula al back

--- public/app/js/administrador/administradorService.js
+++ public/app/js/administrador/administradorService.js
@@ -18,5 +18,44 @@ let administradorService = {
             console.log(response.Error),
             console.error("ERROR EN LA PETICION ", error)
         });
+    },
+    savePelicula: (data) => {
+        return fetch("pelicula/save", {
+            method: "POST", 
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }, 
+            body:JSON.stringify(data)
+        })
+        .then(response => {
+            if(!response.ok){
+                throw new Error(response.status);
+            }
+            return response.json()
+        })
+        .catch(error => {
+            console.log(response.Error),
+            console.error("ERROR EN LA PETICION ", error)
+        });
+    },
+    ordenar: () => {
+        return fetch("sala/ordenamiento", {
+            method: "POST", 
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        })
+        .then(response => {
+            if(!response.ok){
+                throw new Error(response.status);
+            }
+            return response.json()
+        })
+        .catch(error => {
+            console.log(response.Error),
+            console.error("ERROR EN LA PETICION ", error)
+        });
     }
 }
